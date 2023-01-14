a2324a03a789650aee3b2923cdcef7f5704fca19
DeltaBoss <delta_62@msn.com>
2023-01-15 00:53:53 +0100

    missed smth and var is evil

--- content.js
+++ content.js
@@ -60,15 +60,15 @@ let buildDownloadButtonModal = (closeButton) => {
 let onDownloadClickModal = (event) => {
     // Get image or video relative to button
     
-    var path = event.path || (event.composedPath && event.composedPath());
+    const path = event.path || (event.composedPath && event.composedPath());
     if(!path) {
       console.log("Unable to get path information from browser.");
       return;
     }
     
     const downloadLink = path[2].querySelector(".video")?.src ||
-        event.path[2].querySelectorAll(".image")[1]?.src ||
-        event.path[2].querySelectorAll(".image")[0]?.src;
+        path[2].querySelectorAll(".image")[1]?.src ||
+        path[2].querySelectorAll(".image")[0]?.src;
 
     const feedUsername = "fansly";
 
